ce4abca668777dceeba52da6557e28569a2899f3
AndreaRomano02 <andy.romano2002@gmail.com>
2023-08-28 16:01:49 +0200

    Add edit form and route with button

---
+++ resources/views/comics/edit.blade.php
@@ -0,0 +1,61 @@
+@extends('layouts.main')
+
+@section('title', 'Edit Comic')
+@dd($comic->title);
+@section('main')
+    <div class="container">
+        <form action="{{ route('comics.store') }}" method="POST" class="row mt-5">
+            @csrf
+
+            <div class="mb-3 col-6">
+                <label for="title" class="form-label">Titolo</label>
+                <input type="text" class="form-control" name="title" id="title" placeholder="Inserisci il titolo..."
+                    required>
+            </div>
+            <div class="mb-3 col-6">
+                <label for="type" class="form-label">Tipo</label>
+                <input type="text" class="form-control" name="type" id="type" placeholder="Inserisci il tipo..."
+                    required>
+            </div>
+            <div class="mb-3 col-6">
+                <label for="series" class="form-label">Serie</label>
+                <input type="text" class="form-control" name="series" id="series" placeholder="Inserisci le serie..."
+                    required>
+            </div>
+            <div class="mb-3 col-3">
+                <label for="price" class="form-label">Prezzo</label>
+                <input type="text" class="form-control" name="price" id="price"
+                    placeholder="Inserisci il prezzo..." required>
+            </div>
+            <div class="mb-3 col-6">
+                <label for="thumb" class="form-label">Immagine</label>
+                <input type="url" class="form-control" name="thumb" id="thumb"
+                    placeholder="Inserisci l'url dell'immagine" required>
+            </div>
+            <div class="mb-3 col-3">
+                <label for="sale_date" class="form-label">Sale Date</label>
+                <input type="date" class="form-control" name="sale_date" id="sale_date" required>
+            </div>
+            <div class="mb-3 col-6">
+                <label for="artists" class="form-label">Artits</label>
+                <input type="text" class="form-control" name="artists" id="artists"
+                    placeholder="Inserisci gli artisti " required>
+            </div>
+            <div class="mb-3 col-6">
+                <label for="writers" class="form-label">Writers</label>
+                <input type="text" class="form-control" name="writers" id="writers"
+                    placeholder="Inserisci gli scrittori" required>
+            </div>
+
+            <div class="col-12 mb-3">
+                <label for="description" class="form-label">Descrizione</label>
+                <textarea class="form-control" name="description" id="description" rows="3"></textarea>
+            </div>
+
+            <div class="text-end">
+                <button type="reset" class="btn btn-danger ">Reset</button>
+                <button class="btn btn-success">Modifica</button>
+            </div>
+        </form>
+    </div>
+@endsection
