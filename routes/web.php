7b15bd675eb7a5d2c760be325752428daf505741
AndreaRomano02 <andy.romano2002@gmail.com>
2023-08-29 14:47:42 +0200

    Fix Web.php

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('comic', ComicController::class);
+Route::get('/', [HomeController::class, 'home'])->name('home');
+
+// Route::resource('comics', ComicController::class);
+
+// Route Comics
+Route::get('/comics', [ComicController::class, "index"])->name("comics.index");
+
+// Route on Create Page
+
+Route::get('/comics/create', [ComicController::class, "create"])->name("comics.create");
+
+// Route on edit
+
+Route::get('/comics/{comic}/edit', [ComicController::class, "edit"])->name("comics.edit");
+
+// Route on Store 
+
+Route::post('/comics', [ComicController::class, "store"])->name("comics.store");
+
+// Route Single Comic
+
+Route::get('/comics/{comic}', [ComicController::class, "show"])->name("comics.show");
+
+// Route on Update
+
+Route::put('/comics/{comic}', [ComicController::class, "update"])->name("comics.update");
+
+
+// Route Destroy
+
+Route::delete('/comics/{comic}', [ComicController::class, "destroy"])->name("comics.destroy");
